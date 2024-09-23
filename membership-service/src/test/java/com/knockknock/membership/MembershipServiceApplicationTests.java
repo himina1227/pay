99cefe5ae99aea459d3fd64e26f6453210d18d2b
himina1227 <himina1227@gmail.com>
2024-09-23 23:36:29 +0900

    - membership 모듈 추가 및 아키텍쳐 설정

---
+++ membership-service/src/test/java/com/knockknock/membership/MembershipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knockknock.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
